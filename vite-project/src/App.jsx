d3f210cbfa570312f86d1685626bf045a218c229
Aleksandr <aleksvarlaam@gmail.com>
2024-03-16 19:04:08 +0200

    Added Tailwind CSS

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
 import PostsList from "./features/posts/PostsList.jsx";
 
 function App() {
 
     return (
         <>
-            <h1>Hello to IndieDev</h1>
+            <h1 className="text-3xl font-bold underline">
+                You are welcome to IndieVar development
+            </h1>
             <PostsList/>
         </>
     )
