f2eadb31ca63208c0cb329f2084ce1a453a698a0
Aleksandr <aleksvarlaam@gmail.com>
2024-04-11 17:38:33 +0300

    Installed i18n initial with demo messages

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,9 +3,12 @@ import * as ReactDOM from "react-dom/client";
 import "./assets/index.css";
 import {router} from "./config/router.jsx";
 import {RouterProvider} from "react-router-dom";
+import "./i18n/config.ts";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <RouterProvider router={router}/>
+        </React.Suspense>
     </React.StrictMode>
 );
--- vite-project/src/pages/home/HomePage.jsx
+++ vite-project/src/pages/home/HomePage.jsx
@@ -1,10 +1,15 @@
+import {useTranslation} from "react-i18next";
 
 const HomePage = () => {
+    const { t } = useTranslation();
 
     return (
-        <h1 className="text-3xl font-bold underline">
-            You are welcome to IndieVar development <br/>
-        </h1>
+        <div className="...">
+            <h1 className="text-3xl font-bold underline">
+                You are welcome to IndieVar development <br/>
+            </h1>
+            <h2>{t("hello_world")}</h2>
+        </div>
     )
 }
 export default HomePage;
