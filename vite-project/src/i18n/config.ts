cc5b4ef206b5d6d6b2370c6df4feed02e190681e
Aleksandr <aleksvarlaam@gmail.com>
2024-04-11 17:48:33 +0300

    Added LocaleSwitcher component

--- vite-project/src/i18n/config.ts
+++ vite-project/src/i18n/config.ts
@@ -1,37 +1,21 @@
 // Core i18next library.
 import i18n from "i18next";
 import HttpApi from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
+import {initReactI18next} from "react-i18next";
+
+export const supportedLngs = {
+    en: "English",
+    ru: "Russian",
+};
 
 i18n
-    // Add React bindings as a plugin.
     .use(HttpApi)
     .use(initReactI18next)
-    // Initialize the i18next instance.
     .init({
-        // Config options
-
-        // Specifies the default language (locale) used
-        // when a user visits our site for the first time.
-        // We use English here, but feel free to use
-        // whichever locale you want.
         lng: "en",
-
-        // Fallback locale used when a translation is
-        // missing in the active locale. Again, use your
-        // preferred locale here.
         fallbackLng: "en",
-
-        // Enables useful output in the browser’s
-        // dev console.
+        supportedLngs: Object.keys(supportedLngs),
         debug: true,
-
-        // Normally, we want `escapeValue: true` as it
-        // ensures that i18next escapes any code in
-        // translation messages, safeguarding against
-        // XSS (cross-site scripting) attacks. However,
-        // React does this escaping itself, so we turn
-        // it off in i18next.
         interpolation: {
             escapeValue: false,
         },
