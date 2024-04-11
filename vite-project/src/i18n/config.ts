2c30980b25539b847687ab66d333db6238cb2b40
Aleksandr <aleksvarlaam@gmail.com>
2024-04-11 18:29:05 +0300

    Added i18n language detector

--- vite-project/src/i18n/config.ts
+++ vite-project/src/i18n/config.ts
@@ -1,17 +1,20 @@
 // Core i18next library.
 import i18n from "i18next";
 import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 import {initReactI18next} from "react-i18next";
 
 export const supportedLngs = {
     en: "English",
-    ru: "Russian",
+    ru: "Русский",
 };
 
 i18n
     .use(HttpApi)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        // Comment lng param to turn on automatically language detect
         lng: "en",
         fallbackLng: "en",
         supportedLngs: Object.keys(supportedLngs),
